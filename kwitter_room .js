2b67e355585df34b24ffd89659f99ca0c5592382
AdiCodeMan <123280392+AdiCodeMan@users.noreply.github.com>
2023-06-28 17:44:35 -0700

    Add files via upload

---
+++ kwitter_room .js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCXMbDIiWoNpw95jpQqMCPbsPuOBcgKWNo",
+    authDomain: "kwitter-fe5c6.firebaseapp.com",
+    databaseURL: "https://kwitter-fe5c6-default-rtdb.firebaseio.com",
+    projectId: "kwitter-fe5c6",
+    storageBucket: "kwitter-fe5c6.appspot.com",
+    messagingSenderId: "91215990572",
+    appId: "1:91215990572:web:6405d98fac194e888f2689"
+  };
+  
+  // Initialize Firebase
+ initializeApp(firebaseConfig);
+
